e9ab02402d327983d976cb3318d39bb751f30ad6
FabriPons <fabriciomartinpons@gmail.com>
2022-12-12 02:15:27 -0300

    primer arreglo

---
+++ app.js
@@ -0,0 +1,6 @@
+const productos = [
+    {nombre: "Dispenser", precio: 1500},
+    {nombre: "Picadora manual", precio: 2000},
+    {nombre: "Angry mama", precio: 1000},
+    {nombre: "Tabla de picar", precio: 1250},   
+]
